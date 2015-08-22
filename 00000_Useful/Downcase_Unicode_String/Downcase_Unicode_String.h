70e7befceba5786e9a16dacfe7de456b75f980ea
Mighten Dai <mighten.dai@gmail.com>
2015-08-22 21:49:00 +0800

    RtlDowncaseUnicodeString

---
+++ 00000_Useful/Downcase_Unicode_String/Downcase_Unicode_String.h
@@ -0,0 +1,40 @@
+﻿//
+//      Downcase_Unicode_String.h
+//
+//    This routine is used for driver,
+//
+//   **** to keep all of the English characters down-case in a memory
+//             ensured by last calling hierarchy.
+//
+//   **** any behavior such as using invalid address can immediately
+//           result in BSOD( Blue Screen Of Death )
+//
+//                     Aug 22, 2015, written by Mighten Dai
+//
+#ifndef    MACRO_PROTECTION__INCLUDE__Downcase_Unicode_String_h__
+#define    MACRO_PROTECTION__INCLUDE__Downcase_Unicode_String_h__
+
+NTSTATUS    DowncaseUnicodeString( PUNICODE_STRING pDes, PUNICODE_STRING pSrc )
+{
+    int       index = 0;
+    const int limit = ( pSrc->Length) / 2 ; // UNICODE String Counted by bytes.
+
+    USHORT *pusSrc = (USHORT *) pSrc->Buffer;
+    USHORT *pusDes = (USHORT *) pDes->Buffer;    
+
+    for ( ; index < limit; ++index )
+    {
+         USHORT temp =  pusSrc[index];
+
+        if ( temp >= 65 && temp <= 90 )
+        {
+            temp |= 0x20;
+        }
+
+		pusDes[index] = temp;
+    }
+
+    return  STATUS_SUCCESS;
+}
+
+#endif // #ifndef    MACRO_PROTECTION__INCLUDE__Downcase_Unicode_String_h__
---
+++ 00000_Useful/Downcase_Unicode_String/ReadMe.txt
@@ -0,0 +1,4 @@
+# Downcase_Unicode_String
+
+DDK 3790.1830 doesn't support calling system Routine
+  RtlDowncaseUnicodeString, I wanna make it.
